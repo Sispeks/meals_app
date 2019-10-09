fb5d8ebb1978af703749d9f8b75746c94cb2bd66
Felix Hoffmann <felixhoffmann@Felixs-Mac-mini.local>
2019-10-09 22:43:05 +0200

    Adding named routes

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // const CategoryMealsScreen(
+  //     {@required this.categoryId, @required this.categoryTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArguments =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    var categoryTitle = routeArguments['title'];
+    var categoryId = routeArguments['id'];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          categoryTitle,
+        ),
+      ),
+      body: Center(
+        child: Text('The recipes for the category.'),
+      ),
+    );
+  }
+}
